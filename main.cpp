7f33cc07f0ad4f4a113f8f5ff4d09677a5008243
Zerohclmax <58201626+Zerohclmax@users.noreply.github.com>
2020-06-06 23:37:22 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<stack>
+#include <list>
+
+using namespace std;
+
+int main() {
+    stack<int> s;
+    string *str = new string[10];
+    scanf("%c", &str);
+//    scanf("%c", &str[1]);
+//    printf("%c",str);
+    cout <<str[0]<<endl;
+//    if (c == ")") {
+//
+//    }
+    return 0;
+
+
+}
